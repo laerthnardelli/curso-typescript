224fca7121161fa941bcfee861e80eb97e0d31b6
laerthnardelli <laerthnardelli@gmail.com>
2021-08-03 14:25:28 -0300

    let & const #02

--- typescript/04-ECMAScript/ecmascript.ts
+++ typescript/04-ECMAScript/ecmascript.ts
@@ -1,4 +1,4 @@
-//let & const
+//let & const #01
 let seraQuePode = '?';
 console.log(seraQuePode);
 
@@ -11,3 +11,24 @@ if (estaFrio) {
 const cpf: string = '123.456.000-99';
 //cpf = '789.101.132-78';
 console.log(cpf);
+
+//let & const #02
+
+var segredo = 'externo!';
+function revelar() {
+  let segredo = 'interno'; //escopo de função, pode usar let ou var
+  console.log(segredo);
+}
+revelar();
+console.log(segredo);
+
+{
+  const def = 'def'; //escopo de bloco
+  console.log(def);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+//console.log(i);
+
