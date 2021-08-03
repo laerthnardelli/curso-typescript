455859f28ea44a6aeec70be037f3bcd13e09818c
laerthnardelli <laerthnardelli@gmail.com>
2021-08-03 14:17:35 -0300

    Let e Const #01

--- typescript/03-Compilador/compilador.js
+++ typescript/03-Compilador/compilador.js
@@ -1,11 +1,11 @@
 "use strict";
-var canal = 'Gaveta';
-var inscritos = 610234;
+let canal = 'Gaveta';
+let inscritos = 610234;
 //canal = inscritos
-console.log("Canal = " + canal);
+console.log(`Canal = ${canal}`);
 //let nome: string = 'Pedro'; //da erro porque já foi declaro em outro aruivo, tudo dentro do mesmo escopo
 nome = 'pedro';
-console.log("Nome = " + nome);
+console.log(`Nome = ${nome}`);
 // (function () {
 //   let nome: string = 'Ana';
 //   console.log(`Nome = ${nome}`); //se refere a um contexto diferete,
@@ -14,12 +14,12 @@ function soma(a, b) {
     return a + b;
 }
 // noImplicitAny
-var qualquerCoisa;
+let qualquerCoisa;
 qualquerCoisa = 12;
 qualquerCoisa = 'abc';
 //"strictNullChecks", "noUnusedParameters" e "noUnusedLocals"
 function saudar(isManha) {
-    var saudacao;
+    let saudacao;
     if (isManha) {
         saudacao = 'Bom dia!';
     }
---
+++ typescript/04-ECMAScript/ecmascript.ts
@@ -0,0 +1,13 @@
+//let & const
+let seraQuePode = '?';
+console.log(seraQuePode);
+
+let estaFrio = true;
+if (estaFrio) {
+  let acao = 'Colocar o casaco!';
+  console.log(acao);
+}
+
+const cpf: string = '123.456.000-99';
+//cpf = '789.101.132-78';
+console.log(cpf);
