971cef7aede6ba37bd0a7cf9d0dc6fe02a3389ae
laerthnardelli <laerthnardelli@gmail.com>
2021-08-02 22:41:40 -0300

    Changing the Target (Version of JavaScript)

--- typescript/03-Compilador/compilador.js
+++ typescript/03-Compilador/compilador.js
@@ -1,11 +1,11 @@
 "use strict";
-var canal = 'Gaveta';
-var inscritos = 610234;
+let canal = 'Gaveta';
+let inscritos = 610234;
 //canal = inscritos
-console.log("Canal = " + canal);
+console.log(`Canal = ${canal}`);
 //let nome: string = 'Pedro'; //da erro porque já foi declaro em outro aruivo, tudo dentro do mesmo escopo
 nome = 'pedro';
-console.log("Nome = " + nome);
+console.log(`Nome = ${nome}`);
 // (function () {
 //   let nome: string = 'Ana';
 //   console.log(`Nome = ${nome}`); //se refere a um contexto diferete,
