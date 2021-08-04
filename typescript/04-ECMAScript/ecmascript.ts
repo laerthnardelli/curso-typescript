e7f0505622353e06772643161f354bbe387782a7
laerthnardelli <laerthnardelli@gmail.com>
2021-08-04 20:16:23 -0300

    Exercise - convert from JS to TS

--- typescript/04-ECMAScript/ecmascript.ts
+++ typescript/04-ECMAScript/ecmascript.ts
@@ -172,3 +172,76 @@ Notificações: ${parseInt(notificacoes) > 9 ? '+9' : notificacoes}
 console.log(boasVindas);
 console.log(`${(1 + 1) * 30}`);
 console.log(`Motor: ${caracteristicas[0]}`);
+
+
+
+// Exercicios - Traduza esses códigos de JavaScript para TypeScript
+
+// Exercicio 1 - Abaixo temos um código em JavaScript. "Traduza-o" para TypeScript!
+// var dobro = function(valor) {
+//   return valor * 2;
+//   }
+//   console.log(dobro(10));
+
+const dobro = (valor: number): number => valor * 2;
+console.log(dobro(10));
+
+// Exercicio 2 - Verifique se há espaço para melhorias nesse trecho de código!
+
+// var dizerOla = function (nome) {
+//   if (nome === undefined) { nome = "Pessoa" }
+//   console.log("Ola, " + nome);
+//   }
+//   dizerOla();
+//   dizerOla("Anna");
+
+const dizerOla = function (nome: string = 'Pessoa'): void {
+  console.log('Olá, ' + nome);
+}
+
+dizerOla();
+dizerOla('Anna');
+
+// Exercicio 3 - Dado esse array, imprima o menor valor!
+
+// var nums = [-3, 33, 38, 5];
+// console.log('???');
+
+const nums = [-3, 33, 38, 5];
+// Imprimir o menor valor
+console.log(Math.min(...nums));
+
+// Exercicio 4 - Adicione os elementos de nums em array !
+
+// var nums = [-3, 33, 38, 5];
+// var array = [55, 20];
+// console.log(array);
+
+
+const array = [55, 20];
+// Adicionar todos os elementos de "nums" em array
+array.push(...nums);
+console.log(array);
+
+// Exercicio 5 - Simplifique os trechos de código abaixo utilizando o operador Destructuring!
+
+// var notas = [8.5, 6.3, 9.4];
+// var notas1 = notas[0];
+// var notas2 = notas[1];
+// var notas3 = notas[2];
+// console.log(nota1, nota2, nota3);
+
+const notas = [8.5, 6.3, 9.4];
+const [nota1, nota2, nota3] = notas;
+console.log(nota1, nota2, nota3);
+
+// Exercicio 6 - Simplifique os trechos de código abaixo utilizando o operador Destructuring!
+
+// var cientista = {primeiroNome: "Will", experiencia: 12};
+// var primeiroNome = cientista.primeiroNome;
+// var experiencia = cientista.experiencia;
+// console.log(primeiroNome, experiencia);
+
+const cientista = { primeiroNome: 'Will', expeciencia: 12 };
+const { primeiroNome, expeciencia } = cientista;
+console.log(primeiroNome, expeciencia);
