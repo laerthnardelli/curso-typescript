a4716f58a511ba02cf117bd2cd6e25ed32b61c4a
laerthnardelli <laerthnardelli@gmail.com>
2021-08-06 13:37:29 -0300

    Classes & Atributos #02

--- typescript/05-Classes/classes.js
+++ typescript/05-Classes/classes.js
@@ -1,4 +1,5 @@
 "use strict";
+//Classes & Atributos #01
 class Data {
     constructor(dia = 1, mes = 1, ano = 1970) {
         this.dia = dia;
@@ -13,4 +14,19 @@ console.log(aniversario);
 const casamento = new Data; // posso omitir os "()"
 casamento.ano = 2017;
 console.log(casamento);
+//Classes & Atributos #02
+class DataEsperta {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const aniversarioEsperto = new DataEsperta(3, 11, 1991);
+aniversarioEsperto.dia = 4;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+const casamentoEsperto = new DataEsperta; // posso omitir os "()"
+casamentoEsperto.ano = 2017;
+console.log(casamentoEsperto);
 //# sourceMappingURL=classes.js.map
--- typescript/05-Classes/classes.ts
+++ typescript/05-Classes/classes.ts
@@ -1,3 +1,5 @@
+
+//Classes & Atributos #01
 class Data {
   // Público por padrão
   dia: number;
@@ -19,3 +21,21 @@ console.log(aniversario);
 const casamento = new Data;// posso omitir os "()"
 casamento.ano = 2017;
 console.log(casamento);
+
+//Classes & Atributos #02
+class DataEsperta {
+  constructor(
+    public dia: number = 1, 
+    public mes: number = 1, 
+    public ano: number = 1970) {
+  }
+}
+
+const aniversarioEsperto = new DataEsperta(3, 11, 1991);
+aniversarioEsperto.dia = 4;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+
+const casamentoEsperto = new DataEsperta; // posso omitir os "()"
+casamentoEsperto.ano = 2017;
+console.log(casamentoEsperto);
