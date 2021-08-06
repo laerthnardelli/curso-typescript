516046fbb89313e7bd8f13b5b7432e698e5706e5
laerthnardelli <laerthnardelli@gmail.com>
2021-08-06 14:14:07 -0300

    Exercise Classes & Methods

--- typescript/05-Classes/classes.js
+++ typescript/05-Classes/classes.js
@@ -47,18 +47,41 @@ console.log(casamentoEsperto);
 // const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
 // console.log(prod2);
 //Classes & Métodos
+// class Produto {
+//   constructor(
+//     public nome: string,
+//     public preco: number,
+//     public desconto: number = 0) {
+//   }
+//   public resumo(): string {
+//     return `${this.nome} custa R$${this.preco} (${this.desconto * 100}% off)`;
+//   }
+// }
+// const prod1 = new Produto('Caneta Bic Preta', 4.20);
+// prod1.desconto = 0.06;
+// console.log(prod1.resumo());
+// const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
+// console.log(prod2.resumo());
+//Exercício usando o exemplo anterior de Produto - Classes & Métodos:
+//Criar método precoComDesconto
+//Quais são os parâmetros e o retorno?
+//Alternar método resumo para mostrar o preço com desconto
 class Produto {
     constructor(nome, preco, desconto = 0) {
         this.nome = nome;
         this.preco = preco;
         this.desconto = desconto;
     }
+    // tb é public
+    precoComDesconto() {
+        return this.preco * (1 - this.desconto);
+    }
     resumo() {
-        return `${this.nome} custa R$${this.preco} (${this.desconto * 100}% off)`;
+        return `${this.nome} custa R$${this.precoComDesconto().toFixed(2)} (${this.desconto * 100}% off)`;
     }
 }
 const prod1 = new Produto('Caneta Bic Preta', 4.20);
-prod1.desconto = 0.06;
+prod1.desconto = 0.05;
 console.log(prod1.resumo());
 const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
 console.log(prod2.resumo());
--- typescript/05-Classes/classes.ts
+++ typescript/05-Classes/classes.ts
@@ -64,24 +64,53 @@ console.log(casamentoEsperto);
 
 //Classes & Métodos
 
+// class Produto {
+//   constructor(
+//     public nome: string,
+//     public preco: number,
+//     public desconto: number = 0) {
+//   }
+
+//   public resumo(): string {
+//     return `${this.nome} custa R$${this.preco} (${this.desconto * 100}% off)`;
+//   }
+// }
+
+// const prod1 = new Produto('Caneta Bic Preta', 4.20);
+// prod1.desconto = 0.06;
+// console.log(prod1.resumo());
+
+// const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
+// console.log(prod2.resumo());
+
+
+
+//Exercício usando o exemplo anterior de Produto - Classes & Métodos:
+//Criar método precoComDesconto
+//Quais são os parâmetros e o retorno?
+//Alternar método resumo para mostrar o preço com desconto
+
 class Produto {
   constructor(
     public nome: string,
     public preco: number,
     public desconto: number = 0) {
   }
+  // tb é public
+  precoComDesconto(): number {
+    return this.preco * (1 - this.desconto);
+  }
 
   public resumo(): string {
-    return `${this.nome} custa R$${this.preco} (${this.desconto * 100}% off)`;
+    return `${this.nome} custa R$${this.precoComDesconto().toFixed(2)} (${this.desconto * 100}% off)`;
   }
 }
 
 const prod1 = new Produto('Caneta Bic Preta', 4.20);
-prod1.desconto = 0.06;
+prod1.desconto = 0.05;
 console.log(prod1.resumo());
 
 const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
 console.log(prod2.resumo());
-
 
 
