163ee2d8cd1fe3079b6e95c3fbfd8f006200d15c
laerthnardelli <laerthnardelli@gmail.com>
2021-08-06 13:52:38 -0300

    Example: class product

--- typescript/05-Classes/classes.js
+++ typescript/05-Classes/classes.js
@@ -29,4 +29,21 @@ console.log(aniversarioEsperto);
 const casamentoEsperto = new DataEsperta; // posso omitir os "()"
 casamentoEsperto.ano = 2017;
 console.log(casamentoEsperto);
+// Exmeplo Classe Produto
+// Atributos: nome, preco e desconto
+// Criar o construtor
+// Obs 1.: Desconto é opcional (valor padrão 0)
+// Obs 2.: Criar dois produtos: passando dois e três params
+class Produto {
+    constructor(nome, preco, desconto = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
+const prod1 = new Produto('Caneta Bic Preta', 4.20);
+prod1.desconto = 0.06;
+console.log(prod1);
+const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
+console.log(prod2);
 //# sourceMappingURL=classes.js.map
--- typescript/05-Classes/classes.ts
+++ typescript/05-Classes/classes.ts
@@ -25,8 +25,8 @@ console.log(casamento);
 //Classes & Atributos #02
 class DataEsperta {
   constructor(
-    public dia: number = 1, 
-    public mes: number = 1, 
+    public dia: number = 1,
+    public mes: number = 1,
     public ano: number = 1970) {
   }
 }
@@ -39,3 +39,27 @@ console.log(aniversarioEsperto);
 const casamentoEsperto = new DataEsperta; // posso omitir os "()"
 casamentoEsperto.ano = 2017;
 console.log(casamentoEsperto);
+
+// Exmeplo Classe Produto
+// Atributos: nome, preco e desconto
+// Criar o construtor
+// Obs 1.: Desconto é opcional (valor padrão 0)
+// Obs 2.: Criar dois produtos: passando dois e três params
+
+class Produto {
+  constructor(
+    public nome: string,
+    public preco: number,
+    public desconto: number = 0) {
+  }
+}
+
+const prod1 = new Produto('Caneta Bic Preta', 4.20);
+prod1.desconto = 0.06;
+console.log(prod1);
+
+const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
+console.log(prod2);
+
+
+
