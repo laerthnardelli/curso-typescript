1e0ba6941ac0f55a515eb41f652a54548fce1f29
laerthnardelli <laerthnardelli@gmail.com>
2021-08-09 21:24:49 -0300

    Access Modifiers - Private

--- typescript/05-Classes/classes.ts
+++ typescript/05-Classes/classes.ts
@@ -114,3 +114,53 @@ const prod2 = new Produto('Caderno Escolar', 18.80, 0.15);
 console.log(prod2.resumo());
 
 
+//Modificadores de Acesso - Private
+
+class Carro {
+  private velocidadeAtual: number = 0
+
+  constructor(
+    public marca: string,
+    public modelo: string,
+    private velocidadeMaxima: number = 200) {
+  }
+
+  private alterarVelocidade(delta: number): number {
+    const novaVelocidade = this.velocidadeAtual + delta;
+    const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+
+    if (velocidadeValida) {
+      this.velocidadeAtual = novaVelocidade;
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+    }
+    return this.velocidadeAtual;
+  }
+
+  public acelerar(): number {
+    return this.alterarVelocidade(5);
+  }
+
+  public frear(): number {
+    return this.alterarVelocidade(-5);
+  }
+}
+
+const carro1 = new Carro('Ford', 'Ka', 185);
+
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+
+Array(40).fill(0).forEach(() => carro1.frear());
+console.log(carro1.frear());
+
+
+// simular "erros"
+// carro1.velocidadeAtual = 300;
+// console.log('atual -> ' + carro1.velocidadeAtual);
+
+// carro1.velocidadeMaxima = 500;
+// console.log('máxima -> ' + carro1.velocidadeMaxima);
+
+// carro1.alterarVelocidade(150)
+// console.log('atual -> ' + carro1.velocidadeAtual);
