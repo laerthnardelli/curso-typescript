f7d616561db83f7fb9df57b8f4de459c130569af
laerthnardelli <laerthnardelli@gmail.com>
2021-08-10 19:59:34 -0300

    Static Attributes and Methods

--- typescript/05-Classes/classes.js
+++ typescript/05-Classes/classes.js
@@ -198,4 +198,15 @@ pessoa1.idade = 10;
 console.log(pessoa1.idade);
 pessoa1.idade = -3;
 console.log(pessoa1.idade);
+// Atributos e métodos estáticos
+class Matematica {
+    static areaCirc(raio) {
+        return Matematica.PI * raio * raio;
+    }
+}
+Matematica.PI = 3.1416;
+// const m1 = new Matematica();
+// m1.PI = 4.2;
+// console.log(m1.areaCirc(4));
+console.log(Matematica.areaCirc(4));
 //# sourceMappingURL=classes.js.map
--- typescript/05-Classes/classes.ts
+++ typescript/05-Classes/classes.ts
@@ -260,3 +260,20 @@ console.log(pessoa1.idade);
 
 pessoa1.idade = -3;
 console.log(pessoa1.idade);
+
+
+// Atributos e métodos estáticos
+class Matematica {
+  static PI: number = 3.1416;
+
+  static areaCirc(raio: number): number {
+      return Matematica.PI * raio * raio;
+  }
+}
+
+// const m1 = new Matematica();
+// m1.PI = 4.2;
+// console.log(m1.areaCirc(4));
+
+console.log(Matematica.areaCirc(4));
+
