a96fce28f03d1e817ff4c2d60a85c4f64b5720c5
laerthnardelli <laerthnardelli@gmail.com>
2021-08-19 21:48:35 -0300

    Namespaces in Multiple Files

--- typescript/06-Namespaces/namespaces.js
+++ typescript/06-Namespaces/namespaces.js
@@ -37,22 +37,25 @@ console.log(PI);
 //Namespaces Aninhados
 console;
 console.log('Namespaces Aninhados');
-var Geometria;
-(function (Geometria) {
-    let Area;
-    (function (Area) {
-        const PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
+// namespace Geometria {
+//   export namespace Area {
+//     const PI = 3.14;
+//     export function circunferencia(raio: number): number {
+//       return PI * Math.pow(raio, 2);
+//     }
+//     export function retangulo(base: number, altura: number): number {
+//       return base * altura;
+//     }
+//   }
+// }
+// console.log(Geometria.Area.circunferencia(10));
+// console.log(Geometria.Area.retangulo(10, 20));
+// console.log(PI);
+//Namespaces em Múltiplos Arquivos
+console.log('Namespaces em Múltiplos Arquivos');
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRect.ts"/>
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
-console.log(PI);
+//tsc -w --outFile namespaces.js geometriaCirc.ts geomatriaRect.ts namespaces.ts
 //# sourceMappingURL=namespaces.js.map
