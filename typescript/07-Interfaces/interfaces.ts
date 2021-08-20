97478a8e50c0a9ebee9d4be173757d919f372846
laerthnardelli <laerthnardelli@gmail.com>
2021-08-20 16:52:22 -0300

    Using interfaces to extend object

--- typescript/07-Interfaces/interfaces.ts
+++ typescript/07-Interfaces/interfaces.ts
@@ -174,3 +174,31 @@ abstract class AbstrataABD implements A, B {
   abstract d(): void;
 }
 
+
+
+// Uso de Interfaces para estender Object
+
+interface Object {
+  log(): void;
+}
+
+Object.prototype.log = function() {
+  console.log(this.toString());
+}
+
+const x = 2;
+const y = 3;
+const z = 4;
+
+x.log();
+y.log();
+z.log();
+
+const cli = {
+  nome: 'Pedro',
+  toString() {
+      return  this.nome;
+  }
+}
+cli.log();
+
