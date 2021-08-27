80556440be7310a3b874c47d9cc7d71c44eab2e7
laerthnardelli <laerthnardelli@gmail.com>
2021-08-27 14:12:56 -0300

    Using Generics with Array #01

--- typescript/08-Genericos/genericos.js
+++ typescript/08-Genericos/genericos.js
@@ -12,4 +12,10 @@ function echoMelhorado(objeto) {
 console.log(echoMelhorado('João').length);
 console.log(echoMelhorado(27));
 console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome);
+// Usando Generics com Array #01 - Generics disponíveis na API
+console.log('Usando Generics com Array #01');
+const avaliacoes = [10, 9.3, 7.7]; // a classe array foi definida como generica, porem na hora de usar eu especifico o tipo (amarro o tipo).
+avaliacoes.push(8.4);
+// avaliacoes.push('5.5');
+console.log(avaliacoes);
 //# sourceMappingURL=genericos.js.map
--- typescript/08-Genericos/genericos.ts
+++ typescript/08-Genericos/genericos.ts
@@ -14,3 +14,12 @@ function echoMelhorado<TIPO>(objeto: TIPO): TIPO {
 console.log(echoMelhorado('João').length);
 console.log(echoMelhorado<number>(27));
 console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome);
+
+
+// Usando Generics com Array #01 - Generics disponíveis na API
+console.log('Usando Generics com Array #01');
+
+const avaliacoes: Array<number> = [10, 9.3, 7.7]; // a classe array foi definida como generica, porem na hora de usar eu especifico o tipo (amarro o tipo).
+avaliacoes.push(8.4);
+// avaliacoes.push('5.5');
+console.log(avaliacoes);
