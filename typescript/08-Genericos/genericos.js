dafce7a599b214e3b9ec379d4acac0b740da6da0
laerthnardelli <laerthnardelli@gmail.com>
2021-08-27 14:18:07 -0300

    Using Generics with Array #02

--- typescript/08-Genericos/genericos.js
+++ typescript/08-Genericos/genericos.js
@@ -18,4 +18,22 @@ const avaliacoes = [10, 9.3, 7.7]; // a classe array foi definida como generica,
 avaliacoes.push(8.4);
 // avaliacoes.push('5.5');
 console.log(avaliacoes);
+// Usando Generics com Array #02
+console.log('Usando Generics com Array #02');
+function imprimir(args) {
+    args.forEach(elemento => console.log(elemento));
+}
+imprimir([1, 2, 3]);
+imprimir([1, 2, 3]);
+imprimir(['Ana', 'Bia', 'Carlos']);
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+]);
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+]);
 //# sourceMappingURL=genericos.js.map
--- typescript/08-Genericos/genericos.ts
+++ typescript/08-Genericos/genericos.ts
@@ -23,3 +23,27 @@ const avaliacoes: Array<number> = [10, 9.3, 7.7]; // a classe array foi definida
 avaliacoes.push(8.4);
 // avaliacoes.push('5.5');
 console.log(avaliacoes);
+
+
+// Usando Generics com Array #02
+console.log('Usando Generics com Array #02');
+
+function imprimir<T>(args: T[]) {
+  args.forEach(elemento => console.log(elemento));
+}
+
+imprimir([1, 2, 3]);
+imprimir<number>([1, 2, 3]);
+imprimir<string>(['Ana', 'Bia', 'Carlos']);
+imprimir<{ nome: string, idade: number }>([
+  { nome: 'Fulano', idade: 22 },
+  { nome: 'Cicrano', idade: 23 },
+  { nome: 'Beltrano', idade: 24 }
+]);
+
+type Aluno = { nome: string, idade: number };
+imprimir<Aluno>([
+  { nome: 'Fulano', idade: 22 },
+  { nome: 'Cicrano', idade: 23 },
+  { nome: 'Beltrano', idade: 24 }
+]);
