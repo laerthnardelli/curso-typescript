93f5f7b2b5ce1440bdfc9af78abc3549a96f4883
laerthnardelli <laerthnardelli@gmail.com>
2021-08-31 20:14:38 -0300

    Exercise - Class Decorator

--- typescript/09-Decorators/decorators.ts
+++ typescript/09-Decorators/decorators.ts
@@ -99,3 +99,48 @@ interface Eletrodomestico {
 const eletro = new Eletrodomestico()
 eletro.imprimir && eletro.imprimir();
 
+
+
+// Desafio Decorator perfilAdmin
+// type Construtor = { new(...args: any[]): {} }
+// const usuarioLogado = {
+//   nome: 'Guilherme Filho',
+//   email: 'guigui@gmail.com',
+//   admin: false
+// }
+
+// class MudancaAdministrativa {
+//   critico() {
+//       console.log('Algo crítico foi alterado!')
+//   }
+// }
+
+// new MudancaAdministrativa().critico()
+
+type Construtor = { new(...args: any[]): {} }
+
+const usuarioLogado = {
+  nome: 'Guilherme Filho',
+  email: 'guigui@gmail.com',
+  admin: true
+}
+
+@perfilAdmin
+class MudancaAdministrativa {
+  critico() {
+    console.log('Algo crítico foi alterado!');
+  }
+}
+
+new MudancaAdministrativa().critico();
+
+function perfilAdmin<T extends Construtor>(construtor: T) {
+  return class extends construtor {
+    constructor(...args: any[]) {
+      super(...args)
+      if (!usuarioLogado || !usuarioLogado.admin) {
+        throw new Error('Sem permissão!')
+      }
+    }
+  }
+}
