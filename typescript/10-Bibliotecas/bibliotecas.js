9ad345f3aa866ee74b330c57da63a9042c1b7d7e
laerthnardelli <laerthnardelli@gmail.com>
2021-09-08 10:40:27 -0300

    created file "jquery.d.ts" to use declare

--- typescript/10-Bibliotecas/bibliotecas.js
+++ typescript/10-Bibliotecas/bibliotecas.js
@@ -1,4 +1,8 @@
 "use strict";
 //01 - Declarando Variável com "Declare" 
+// declare const $: any
+// $('body').append('Usando jQuery')
+//02 -  Entendendo os Arquivos de Declaração de Tipos
+//criando um arquivo a parte chamado jquery.d.ts, para utlizar o declare const $: any
 $('body').append('Usando jQuery');
 //# sourceMappingURL=bibliotecas.js.map
--- typescript/10-Bibliotecas/bibliotecas.ts
+++ typescript/10-Bibliotecas/bibliotecas.ts
@@ -1,6 +1,14 @@
 
 //01 - Declarando Variável com "Declare" 
 
-declare const $: any
+// declare const $: any
+
+// $('body').append('Usando jQuery')
+
+
+//02 -  Entendendo os Arquivos de Declaração de Tipos
+
+//criando um arquivo a parte chamado jquery.d.ts, para utlizar o declare const $: any
 
 $('body').append('Usando jQuery')
+
