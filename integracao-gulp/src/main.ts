5e0918e2cc36f5a072e08d732321234a49538f5f
laerthnardelli <laerthnardelli@gmail.com>
2021-09-08 11:47:34 -0300

    Using a JavaScript Library (Jquery)

--- integracao-gulp/src/main.ts
+++ integracao-gulp/src/main.ts
@@ -1,11 +1,25 @@
 
 //https://www.typescriptlang.org/docs/handbook/gulp.html
 
-import Livro from './modelo/livro';
+// import Livro from './modelo/livro';
 
-const livro = new Livro('Dom Quixote', 108.80, 0.10);
-console.log(livro.precoComDesconto());
+// const livro = new Livro('Dom Quixote', 108.80, 0.10);
+// console.log(livro.precoComDesconto());
 
 
 //Configurando a Estrutura do Build
 //npm i --save-dev gulp typescript browserify tsify vinyl-source-stream del
+
+
+//Usando uma Biblioteca JavaScript
+//npm i --save-dev jquery @types/jquery
+
+
+import $ from 'jquery'
+import Livro from './modelo/livro'
+
+const livro = new Livro('Dom Quixote', 108.80, 0.10)
+// console.log(livro.precoComDesconto())
+
+$('body').append(`<h1>${livro.nome}</h1>`);
+$('body').append(`<h2>Preço: R$${livro.precoComDesconto()}</h2>`);
