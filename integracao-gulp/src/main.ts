f50107d300c3a768c260770b23ed971410535236
laerthnardelli <laerthnardelli@gmail.com>
2021-09-08 11:01:25 -0300

    Creating the TypeScript Project for the "Gulp"

---
+++ integracao-gulp/src/main.ts
@@ -0,0 +1,7 @@
+
+//https://www.typescriptlang.org/docs/handbook/gulp.html
+
+import Livro from './modelo/livro';
+
+const livro = new Livro('Dom Quixote', 108.80, 0.10);
+console.log(livro.precoComDesconto());
