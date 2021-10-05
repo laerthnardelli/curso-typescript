69061c81522862428538d930756b5ff9ee623d68
laerthnardelli <laerthnardelli@gmail.com>
2021-10-05 11:12:20 -0300

    Functional Component: Applying Types

--- integracao-react/src/App.tsx
+++ integracao-react/src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ContadorValor from './componentes/ContadorValor';
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-      
+        <ContadorValor contador={111}/>
       </div>
     );
   }
