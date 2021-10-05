44dd0e8b923fbf6ef235c8a829085f1164c84fd8
laerthnardelli <laerthnardelli@gmail.com>
2021-10-05 14:49:01 -0300

    Class Component #02

--- integracao-vue/src/App.vue
+++ integracao-vue/src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
     <Contador :valorInicial="10" />
-    <ContadorClasse />
+    <ContadorClasse :valorInicial="100" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import Contador from '@/componentes/Contador.vue'
-import ContadorClasse from '@/componentes/ContadorClasse.vue'
+import { Component, Vue } from "vue-property-decorator";
+import Contador from "@/componentes/Contador.vue";
+import ContadorClasse from "@/componentes/ContadorClasse.vue";
 
 @Component({
   components: {
-    Contador, ContadorClasse
+    Contador,
+    ContadorClasse,
   },
 })
 export default class App extends Vue {}
